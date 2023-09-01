00bc53ebcf43ece8af0e7af3b3090d3404850837
Nathanv2 <lnathan718@gmail.com>
2023-09-01 02:16:55 -0700

    Update

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 15.0f;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + new Vector3(0.32, 4.89, );
     }
 }
